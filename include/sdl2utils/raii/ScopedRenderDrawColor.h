52e435711fd82ca6f1ab2c74fbd7f7ae7f91823f
Taras Denysenko <terrapass93@gmail.com>
2016-06-21 23:19:22 +0300

    Created raii subnamespace, dedicated to RAII helpers for SDL. Moved ScopedRenderDrawColor and ScopedRenderTarget to this subnamespace from another project.

---
+++ include/sdl2utils/raii/ScopedRenderDrawColor.h
@@ -0,0 +1,49 @@
+#ifndef SCOPEDRENDERCOLOR_H
+#define SCOPEDRENDERCOLOR_H
+
+#include <SDL2/SDL.h>
+
+#include "../guards.h"
+
+namespace sdl2utils
+{
+    namespace raii
+    {
+        using namespace sdl2utils::guards::abbr;
+
+        class ScopedRenderDrawColor final
+        {
+            SDL_Renderer* const prenderer;
+            const SDL_Color oldColor;
+
+        public:
+            ScopedRenderDrawColor(SDL_Renderer* const prenderer, const SDL_Color& newColor)
+                : prenderer(prenderer), oldColor(getRendererDrawColor(prenderer))
+            {
+                z(SDL_SetRenderDrawColor(this->prenderer, newColor.r, newColor.g, newColor.b, newColor.a));
+            }
+
+            ~ScopedRenderDrawColor()
+            {
+                // Not guarding, since destructors mustn't throw
+                SDL_SetRenderDrawColor(prenderer, oldColor.r, oldColor.g, oldColor.b, oldColor.a);
+            }
+
+        private:
+            ScopedRenderDrawColor(const ScopedRenderDrawColor&) = delete;
+            ScopedRenderDrawColor& operator=(const ScopedRenderDrawColor&) = delete;
+
+            static inline SDL_Color getRendererDrawColor(SDL_Renderer* const prenderer)
+            {
+                using namespace sdl2utils::guards::abbr;
+
+                SDL_Color result;
+                z(SDL_GetRenderDrawColor(prenderer, &(result.r), &(result.g), &(result.b), &(result.a)));
+                return result;
+            }
+        };
+    }
+}
+
+#endif /* SCOPEDRENDERCOLOR_H */
+
---
+++ include/sdl2utils/raii/ScopedRenderTarget.h
@@ -0,0 +1,40 @@
+#ifndef SCOPEDRENDERTARGET_H
+#define SCOPEDRENDERTARGET_H
+
+#include <SDL2/SDL.h>
+
+#include "../guards.h"
+
+namespace sdl2utils
+{
+    namespace raii
+    {
+        using namespace sdl2utils::guards::abbr;
+        
+        class ScopedRenderTarget final
+        {
+            SDL_Renderer* const prenderer;
+            SDL_Texture* const poldTarget;
+
+        public:
+            ScopedRenderTarget(SDL_Renderer* const prenderer, SDL_Texture* const pnewTarget)
+                : prenderer(prenderer), poldTarget(SDL_GetRenderTarget(this->prenderer))
+            {
+                z(SDL_SetRenderTarget(prenderer, pnewTarget));
+            }
+
+            ~ScopedRenderTarget()
+            {
+                // Not guarding, since destructors mustn't throw
+                SDL_SetRenderTarget(prenderer, poldTarget);
+            }
+
+        private:
+            ScopedRenderTarget(const ScopedRenderTarget&) = delete;
+            ScopedRenderTarget& operator=(const ScopedRenderTarget&) = delete;
+        };
+    }
+}
+
+#endif /* SCOPEDRENDERTARGET_H */
+
