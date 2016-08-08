7c5398adbfd86ad9542b01219403f3472c4099b4
Taras Denysenko <terrapass93@gmail.com>
2016-08-08 13:39:25 +0300

    Added raii.h header file - a facade, which includes every header file in raii/ subdirectory.

---
+++ include/sdl2utils/raii.h
@@ -0,0 +1,11 @@
+#ifndef RAII_H
+#define RAII_H
+
+#include "raii/ScopedRenderDrawColor.h"
+#include "raii/ScopedRenderTarget.h"
+#include "raii/ScopedSDLCore.h"
+#include "raii/ScopedSDLImageExt.h"
+#include "raii/ScopedSDLTtfExt.h"
+
+#endif /* RAII_H */
+
