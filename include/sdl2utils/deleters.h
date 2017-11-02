3462b995b2a3d5025c0507e01a2ba89eacbfc195
Taras Denysenko <terrapass93@gmail.com>
2017-11-02 22:07:19 +0200

    Added a using declaration for TTF_FontPtr, unique_ptr to TTF_Font.

--- include/sdl2utils/deleters.h
+++ include/sdl2utils/deleters.h
@@ -2,6 +2,7 @@
 #define SDL2UTILS_DELETERS_H
 
 #include <SDL.h>
+#include <SDL_ttf.h>
 
 namespace sdl2utils
 {
@@ -25,6 +26,14 @@ namespace sdl2utils
         inline void operator()(SDL_Surface* psurface) const
         {
             SDL_FreeSurface(psurface);
+        }
+    };
+    
+    struct TTFPtrDeleter
+    {
+        inline void operator()(TTF_Font* pfont) const
+        {
+            TTF_CloseFont(pfont);
         }
     };
 }
--- include/sdl2utils/pointers.h
+++ include/sdl2utils/pointers.h
@@ -14,6 +14,8 @@ namespace sdl2utils
     using SDL_RendererPtr   = unique_ptr<SDL_Renderer, SDL2PtrDeleter>;
     using SDL_TexturePtr    = unique_ptr<SDL_Texture, SDL2PtrDeleter>;
     using SDL_SurfacePtr    = unique_ptr<SDL_Surface, SDL2PtrDeleter>;
+    
+    using TTF_FontPtr       = unique_ptr<TTF_Font, TTFPtrDeleter>;
 }
 
 #endif /* SDL2UTILS_POINTERS_H */
